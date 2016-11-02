347009158a571c645986e08e66bb41ad3c30b81d
Matioma <matioma-98@rambler.ru>
2016-11-02 19:27:37 +0200

    Castle_Defence_ utilities

---
+++ Utilities/Database.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+namespace Assets.Utilities
+{
+    public class Database
+    {
+        public static AllObjectParameters GetValue()
+        {
+            var dataBase = GameObject.FindWithTag("DataBase");
+            return dataBase.gameObject.GetComponent<AllObjectParameters>();
+        }
+
+        public static void SetNotEnoughMoney(bool activeState)
+        {
+            GetValue().NotEnoughMoney.SetActive(activeState);
+        }
+    }
+}
